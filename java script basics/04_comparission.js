0106acc2a4720960e66e914d9e9a718569890afc
SandeshDM <124351572+SandeshDM@users.noreply.github.com>
2024-02-28 21:34:57 +0000

    data types

---
+++ java script basics/04_comparission.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0); // here conversion does not happen of null value only in null rest all cases it happens with ==
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);   // strict check dosen't convert if ===
